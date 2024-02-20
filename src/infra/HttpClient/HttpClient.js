1d04dc41a4039e2876c67638b3e00c80afb65d30
PeuStone <pedrohmotaaa@gmail.com>
2024-02-20 10:43:32 -0300

    Customizando o fetch do projeto.

---
+++ src/infra/HttpClient/HttpClient.js
@@ -0,0 +1,19 @@
+// Arquitetura Hexagonal
+// Ports & Adapters
+export async function HttpClient(fetchUrl, fetchOptions) {
+    const options = {
+        ...fetchOptions,
+        headers: {
+            'Content-Type': 'application/json',
+            ...fetchOptions.headers,
+        },
+        body: fetchOptions.body ? JSON.stringify(fetchOptions.body) : null,
+    }
+    return fetch(fetchUrl, options)
+        .then(async (respostaServidor) => {
+            return {
+                ok: respostaServidor.ok,
+                body: await respostaServidor.json(),
+            }
+        })
+}
--- src/services/auth/authService.js
+++ src/services/auth/authService.js
@@ -1,18 +1,14 @@
+import { HttpClient } from "../../infra/HttpClient/HttpClient";
+
 export const authService = {
     async login({ username, password }) {
-        return fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
+        return HttpClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
+            body: { username, password }
         })
             .then(async (respostaServidor) => {
                 if (!respostaServidor.ok) throw new Error('Usuário ou senha inválidos!');
-                const body = await respostaServidor.json()
+                const body = respostaServidor.body;
                 console.log(body);
             })
     }
